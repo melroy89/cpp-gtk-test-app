06401d7e1a7b31346df4114faf512aa6e31b962f
Melroy van den Berg <melroy@melroy.org>
2022-01-02 01:45:46 +0100

    Formatter + license

--- window.cc
+++ window.cc
@@ -1,9 +1,10 @@
 #include "window.h"
 
-MainWindow::MainWindow() : m_vbox(Gtk::ORIENTATION_VERTICAL),
-                           m_button("Hello World"),
-                           m_menuItem1("File"),
-                           m_menuItem2("Help")
+MainWindow::MainWindow()
+    : m_vbox(Gtk::ORIENTATION_VERTICAL),
+      m_button("Hello World"),
+      m_menuItem1("File"),
+      m_menuItem2("Help")
 {
   set_title("C++ GTK Example App");
   set_default_size(1000, 680);
@@ -27,7 +28,7 @@ MainWindow::MainWindow() : m_vbox(Gtk::ORIENTATION_VERTICAL),
   m_menuItem2.set_submenu(m_submenu2);
   m_menu.append(m_menuItem1);
   m_menu.append(m_menuItem2);
-  
+
   // Add all elements to the horizontal box
   m_hbox.pack_start(m_button, false, false, 4);
   m_hbox.pack_end(m_textview, true, true, 4);
@@ -35,7 +36,7 @@ MainWindow::MainWindow() : m_vbox(Gtk::ORIENTATION_VERTICAL),
   // Add the menu and horizontal box to the verticial box
   m_vbox.pack_start(m_menu, false, false, 0);
   m_vbox.pack_start(m_hbox, false, false, 4);
-  
+
   add(m_vbox);
   show_all_children();
 }
